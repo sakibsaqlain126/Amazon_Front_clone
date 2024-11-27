90355dfe2feab534d11f6151ebff5b4e48ac8655
Sakib E Saqlain <66792541+sakibsaqlain126@users.noreply.github.com>
2024-11-27 18:49:10 +0600

    Add files via upload

---
+++ form.php
@@ -0,0 +1,29 @@
+<?php
+
+
+?>
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+     
+   <form action="" method="post">
+    <label for="" >Name </label> <br>
+    <input type="text" name="name"></
+    > 
+   </form>
+
+
+
+</body>
+</html>
---
+++ index2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <label for="">Name</label> <br>
+        <input type="text'> <br>
+        <label for="">Email</label> <br>
+        <input type="email'> <br>  
+        <label for="">Password</label> <br>
+        <input type="password'><br>
+
+        <input type="submit" value="SubmitData" class=btn><br>
+        
+
+        </form>
+</body>
+</html>
